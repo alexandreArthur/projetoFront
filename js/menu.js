21236e59c4be7c55151030d45d9565a77ae44351
Arthur Alexandre <arthur_dda@hotmail.com>
2021-08-14 12:09:21 -0300

    Menu resolvido bug que o f5 fazia o menu sumir

--- js/menu.js
+++ js/menu.js
@@ -11,7 +11,7 @@ function Menu(config){
     var _this = this;
 
     this.btn.removeAttribute('style');
-    closeMenu();
+    //closeMenu();
     if(this.maxWidth){
         window.addEventListener('resize', e =>{
             if(window.innerWidth > _this.maxWidth){
@@ -21,6 +21,9 @@ function Menu(config){
                 closeMenu();
             }
         });
+        if(window.innerWidth <= _this.maxWidth){
+            closeMenu();
+        }
     }
 
     this.btn.addEventListener('click', openOrClose);
